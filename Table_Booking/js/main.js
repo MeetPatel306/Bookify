83383015dd01f8ea5039079a988354f5cdb40165
meet patel <meetpatel30506@gmail.com>
2025-11-08 13:24:56 +0530

    Initial commit

--- Table_Booking/js/main.js
+++ Table_Booking/js/main.js
@@ -1,123 +1,59 @@
-(function ($) {
-    "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-
-
-    // Initiate the wowjs
+// Initiate the WOW.js library
+if (typeof WOW !== "undefined") {
     new WOW().init();
+}
 
+// Sticky Navbar
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 45) {
+        navbar.classList.add("sticky-top", "shadow-sm");
+    } else {
+        navbar.classList.remove("sticky-top", "shadow-sm");
+    }
+});
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
+// Modal Video
+const btnPlay = document.querySelectorAll(".btn-play");
+let videoSrc = "";
+
+btnPlay.forEach(btn => {
+    btn.addEventListener("click", function () {
+        videoSrc = this.getAttribute("data-src");
+    });
+});
+
+const videoModal = document.getElementById("videoModal");
+const videoElement = document.getElementById("video");
+
+if (videoModal) {
+    videoModal.addEventListener("shown.bs.modal", function () {
+        videoElement.setAttribute("src", videoSrc + "?autoplay=1&modestbranding=1&showinfo=0");
     });
 
-
-    // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-
-    $(window).on("load resize", function () {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-                function () {
-                    const $this = $(this);
-                    $this.addClass(showClass);
-                    $this.find($dropdownToggle).attr("aria-expanded", "true");
-                    $this.find($dropdownMenu).addClass(showClass);
-                },
-                function () {
-                    const $this = $(this);
-                    $this.removeClass(showClass);
-                    $this.find($dropdownToggle).attr("aria-expanded", "false");
-                    $this.find($dropdownMenu).removeClass(showClass);
-                }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
+    videoModal.addEventListener("hide.bs.modal", function () {
+        videoElement.setAttribute("src", "");
     });
+}
 
+// Filter button click handlers
+const filterButtons = document.querySelectorAll('.filter-btn');
+const carousels = document.querySelectorAll('.carousel');
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 0, 'easeInOutExpo');
-        return false;
-    });
+filterButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Update active button
+        filterButtons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
 
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 0.5,
-        time: 1500
-    });
-
-
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
-
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        margin: 24,
-        dots: true,
-        loop: true,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            992: {
-                items: 3
+        // Show selected carousel, hide others
+        const targetCarousel = button.getAttribute('data-target');
+        carousels.forEach(carousel => {
+            if (carousel.id === targetCarousel) {
+                carousel.classList.add('active');
+            } else {
+                carousel.classList.remove('active');
             }
-        }
+        });
     });
-
-})(jQuery);
-
-
-
-
-
+});
